61398fba9fbcbb86bb68c63b2fb38ec8813ddaa7
Lucianofc138 <lucianofc138@gmail.com>
2022-06-29 21:22:43 -0400

    add frame processor
    - Need to clean metro cache.
    - https://github.com/mrousavy/react-native-vision-camera/issues/730

--- App.js
+++ App.js
@@ -5,16 +5,27 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-reanimated';
 import React, {useEffect} from 'react';
 import {StyleSheet, Text} from 'react-native';
-import {Camera, useCameraDevices} from 'react-native-vision-camera';
+import {
+  Camera,
+  useCameraDevices,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
 
 function App() {
   const devices = useCameraDevices();
   const device = devices.front;
 
+  const frameProcessor = useFrameProcessor(frame => {
+    'worklet';
+    const isHotdog = Math.random() > 0.1;
+    console.log(isHotdog ? 'Hotdog!' : 'Not Hotdog.');
+  }, []);
+
   useEffect(() => {
+    console.log('Hermes??: ', !!global.HermesInternal);
     (async () => {
       const newCameraPermission = await Camera.requestCameraPermission();
       const newMicrophonePermission =
@@ -26,7 +37,13 @@ function App() {
     return <Text>Loading...</Text>;
   }
   return (
-    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      frameProcessor={frameProcessor}
+      frameProcessorFps={0.2}
+    />
   );
 }
 
