91b38630ffab09bae901bc0b311cea47b462da89
Lucianofc138 <lucianofc138@gmail.com>
2022-06-30 17:22:16 -0400

    fix face detection and bounding box (for just one face)

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 import 'react-native-reanimated';
 import React, {useState, useEffect} from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {Button, Dimensions, StyleSheet, Text, View} from 'react-native';
 import {
   Camera,
   useCameraDevices,
@@ -19,8 +19,11 @@ import {runOnJS} from 'react-native-reanimated';
 export default function App() {
   const [hasPermission, setHasPermission] = useState(false);
   const [faces, setFaces] = useState(); //Face[]
-  const width = Dimensions.get('window').width;
-  const height = Dimensions.get('window').height;
+
+  const [frameSize, setFrameSize] = useState({height, width});
+  const window = Dimensions.get('window');
+  const width = window?.width || 0;
+  const height = window?.height || 0;
 
   const devices = useCameraDevices();
   const device = devices.front;
@@ -38,35 +41,59 @@ export default function App() {
 
   const frameProcessor = useFrameProcessor(frame => {
     'worklet';
-    console.log([frame.width, frame.height], [width, height]);
+    console.log('------------------------------');
+    console.log('frame: ', frame.toString());
+    const frameHeight = Math.max(frame?.width || 0, frame?.height || 0);
+    const frameWidth = Math.min(frame?.width || 0, frame?.height || 0);
+    const size = {height: frameHeight, width: frameWidth};
+    console.log('frame:', size);
+    console.log('screen', {width, height});
+
+    // console.log('size: ', size);
+    runOnJS(setFrameSize)(size);
     const scannedFaces = scanFaces(frame);
     runOnJS(setFaces)(scannedFaces);
   }, []);
 
   const getROIPosition = () => {
-    const ROITop = Math.floor(faces?.[0]?.bounds?.y) || 0;
+    console.log('frame size: ', frameSize);
+    console.log('size: ', {height, width});
+    const verticalFactor = height / frameSize.height; //Device size vs Frame size for Huawei Mate 20 Pro
     const ROIHeight =
-      Math.floor(faces?.[0]?.bounds?.height) || 10;
+      Math.floor(faces?.[0]?.bounds?.height * verticalFactor) || 10;
+    const ROITop =
+      Math.floor(
+        faces?.[0]?.bounds?.boundingExactCenterY * verticalFactor -
+          ROIHeight / 2,
+      ) || 0;
+    console.log({verticalFactor, ROITop, ROIHeight});
 
-    const ROIRight = Math.floor(faces?.[0]?.bounds?.x) || 0;
-    const ROIWidth = Math.floor(faces?.[0]?.bounds?.width * 755) || 10;
+    const horizontalFactor = width / frameSize.width;
+    const ROIWidth =
+      Math.floor(faces?.[0]?.bounds?.width * horizontalFactor) || 10;
+    const ROIRight =
+      Math.floor(
+        faces?.[0]?.bounds?.boundingExactCenterX * horizontalFactor -
+          ROIWidth / 2,
+      ) || 0;
 
     const verticalValues = {
-      top: Math.max(ROITop, 10),
-      height: Math.max(ROIHeight, 20),
+      top: ROITop,
+      height: ROIHeight,
     };
     console.log('vertical: ', verticalValues);
 
     const horizontalValues = {
-      width: Math.max(ROIWidth, 20),
-      right: Math.max(ROIRight, 20),
+      width: ROIWidth,
+      right: ROIRight,
     };
+    // console.log('horizontal: ', horizontalValues);
 
     const a = {
       ...horizontalValues,
       ...verticalValues,
     };
-    console.log(a.top);
+    // console.log(a.top);
     return a;
   };
 
@@ -74,10 +101,15 @@ export default function App() {
     <>
       <Camera
         style={StyleSheet.absoluteFill}
+        // style={{
+        //   height: 128 * 5,
+        //   width: 72 * 5,
+        // }}
         device={device}
         isActive={true}
         frameProcessor={frameProcessor}
-        frameProcessorFps={1}
+        frameProcessorFps={24}
+        videoStabilizationMode={true}
       />
       <View
         style={{
@@ -87,6 +119,20 @@ export default function App() {
           borderColor: 'blue',
         }}
       />
+      <View style={{flex: 1, justifyContent: 'flex-end'}}>
+        <Button
+          title={'Boton'}
+          onPress={async () => {
+            console.log('Front camera');
+            const photo = await Camera.current.takeSnapshot({
+              quality: 85,
+              skipMetadata: true,
+            });
+            console.log(photo);
+            // console.log(device.formats);
+          }}
+        />
+      </View>
     </>
   ) : (
     <Text>Loading...</Text>
